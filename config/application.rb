e24f2e5bf39d70a7ca2e0ccdbd5418cbc116274b
Jared Williams <jaredwilliamsmail@gmail.com>
2015-03-08 22:31:40 -0500

    initial devise commit

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module BrewPinting
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #for Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
