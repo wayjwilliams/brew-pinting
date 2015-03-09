97354b5227576b856c4416655b15cae345108d7c
Jared Williams <jaredwilliamsmail@gmail.com>
2015-03-09 15:55:44 -0500

    round out user names

--- app/models/pint.rb
+++ app/models/pint.rb
@@ -1,5 +1,10 @@
 class Pint < ActiveRecord::Base
   belongs_to :user
+
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  validates :image, presence: true
+  validates :description, presense: true
+  
 end
